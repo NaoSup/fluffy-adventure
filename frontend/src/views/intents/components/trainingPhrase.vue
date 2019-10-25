d4e68bf021f7efd6f1facac1ade72743dd1f3c5a
Naomi <naomi.paulmin@gmail.com>
2019-10-25 21:18:10 +0200

    create form to add new intent

---
+++ frontend/src/views/intents/components/trainingPhrase.vue
@@ -0,0 +1,31 @@
+<template>
+  <tr @mouseover="isHovered = true" @mouseleave="isHovered = false">
+    <td>{{ phrase }}</td>
+    <td class="text-right">
+      <!-- @TODO put icon instead of "Delete text" -->
+      <button v-show="isHovered" class="btn" @click.prevent="deletePhrase">Delete</button>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'TrainingPhrase',
+  props: {
+    phrase: { type: String, required: true }
+  },
+  data() {
+    return {
+      isHovered: false
+    }
+  },
+  methods: {
+    deletePhrase() {
+      if (this.isHovered) {
+        this.$emit('delete')
+        return
+      }
+    }
+  }
+}
+</script>
---
+++ frontend/src/views/layout/components/navigationLink.vue
@@ -0,0 +1,33 @@
+<template>
+  <li class="nav-item">
+    <router-link :to="route" class="nav-link p-3">
+      <span v-if="name && name.length">{{ name }}</span>
+    </router-link>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'NavigationLink',
+  props: {
+    icon: { type: String, default: '' },
+    name: { type: String, default: '' },
+    route: { type: String, required: true }
+  }
+}
+</script>
+
+<style lang="css">
+.nav-link,
+.fa-icon {
+  color: #fff;
+}
+.nav-link:hover .fa-icon {
+  color: #bababa;
+}
+.nav-link:hover,
+a:hover {
+  text-decoration: none;
+  color: #fff;
+}
+</style>

249880edef84ccc8390ee2633e194c9034253ed6
Naomi <naomi.paulmin@gmail.com>
2019-10-25 09:39:11 +0200

    create intents page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,31 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  // avoid page jumping during vue router transitions
+  scrollBehavior(to, from, savedPosition) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          x: 0,
+          y: 0
+        })
+      }, 280)
+    })
+  },
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      redirect: 'intents'
     },
+    {
+      path: '/intents',
+      name: 'Intents',
+      component: () => import('@/views/intents/index')
+    }
   ],
-});
+})
---
+++ frontend/src/views/intents/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>intents</h1>
+  </div>
+</template>

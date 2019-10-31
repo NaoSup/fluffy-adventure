5b517fab1df26f32cb00c2e7c2ab1988c3108817
Naomi <naomi.paulmin@gmail.com>
2019-10-31 10:22:40 +0100

    db connection + action creation route

---
+++ backend/models/action.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ActionSchema = new Schema(
+  {
+    slug: {
+      type: String,
+      index: true,
+      required: true
+    },
+    isActivated: {
+      type: Boolean,
+      default: true
+    },
+    triggerIntentIds: {
+      type: [
+        {
+          projectId: {
+            type: String,
+            required: true
+          },
+          intentId: {
+            type: String,
+            required: true
+          }
+        }
+      ],
+      default: undefined
+    },
+    labels: {
+      type: [
+        {
+          locale: {
+            type: String,
+            required: true
+          },
+          name: {
+            type: String,
+            required: true
+          }
+        }
+      ]
+    }
+  },
+  {
+    timestamps: true // Use Mongoose createdAt & updatedAt,
+  }
+)
+
+ActionSchema.options.toJSON = {
+  getters: true,
+  transform: function(doc, ret, options) {
+    delete ret.updatedAt
+    delete ret.createdAt
+    delete ret.__v
+    delete ret.createdBy
+    delete ret.dateDeleted
+    return ret
+  }
+}
+
+module.exports = mongoose.model('Action', ActionSchema)

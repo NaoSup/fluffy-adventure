9920c725427db2e66bcf56d7c6d254ba81f75b50
Salhotra Dass <dinesh.salhotradass@ynov.com>
2019-11-04 17:42:21 +0100

    We can trigger a action by the inten

--- backend/controllers/webhook.js
+++ backend/controllers/webhook.js
@@ -1,26 +1,17 @@
+const ActionModel = require('../models/Action')
 class WebHook {
   async getProblem(reqWebHookp) {
     let reqWebHook = reqWebHookp
-    console.log("entre#######")
-    const valideteProblems = ["problem_order", "problem_delivery", "problem_deliverer"]
-    const userintent = reqWebHook.queryResult.intent.displayName
-    console.log(userintent)
-    if (valideteProblems.includes(userintent)) {
-      console.log("ready for action ????")
+    const userintent = reqWebHook.queryResult.intent.name
+    const foundAction = await ActionModel.findOne({
+      'triggerIntentIds.intentId': userintent
+    })
 
-      const textToSend = "Je veux pas savoir tes problemes laisse moi tranquille merde"
-      console.log(reqWebHook)
+    if (foundAction !== null) {
       return {
-
-        "fulfillmentText": textToSend
+        fulfillmentText: foundAction.answers[Math.floor(Math.random() * foundAction.answers.length - 1)]
       }
-      // Oh,  je m'excuse d'avance. Pouvez-vous m' en dire plus sur ce qui s'est passé avec le livreur ?   
     }
-
-    // problem_order, problem_delivery, problem_deliverer
-
-
   }
-
 }
 module.exports = new WebHook()
